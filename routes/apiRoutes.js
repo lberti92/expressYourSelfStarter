10f4c5b10fd075a1ad6e3dc7e9c43b1f10e2e3a9
Breeze <lberti@msn.com>
2019-07-17 19:38:02 -0600

    finished project

---
+++ routes/apiRoutes.js
@@ -0,0 +1,17 @@
+var feelings = require("../data/feelings");
+var response = require("../data/responses");
+
+module.exports = function(app) {
+    app.get("/api/feelings", function(req, res) {
+        res.send(feelings);
+    })
+    
+    app.get("/api/responses", function(req, res) {
+        res.send(response);
+    })
+
+    app.post("/api/response", function(req,res) {
+        response.push(req.body);
+        res.send(true);
+})
+}
